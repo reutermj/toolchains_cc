e79bba10c6aa5e98eb5e3da365f51f8c784b15e9
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2025-09-18 19:25:04 -0700

    fix: remove attestations

--- .github/workflows/create-release.sh
+++ .github/workflows/create-release.sh
@@ -5,7 +5,6 @@ DATE=$(grep -o 'version = "[^"]*"' MODULE.bazel | cut -d '"' -f 2 | head -n 1)
 SRC_TAR="toolchains_cc-$DATE.tar.gz"
 gh release create "$DATE" \
   $SRC_TAR \
-  $SRC_TAR.intoto.jsonl \
   --title "$DATE" \
   --notes "### Installation
 \`\`\`
