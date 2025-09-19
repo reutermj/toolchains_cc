878dbef04f07df37519c4c62c3a5d24ac18816a6
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2025-09-18 17:08:44 -0700

    fix: update release workflow to produce source tarball with attestation (#7)

---
+++ .github/workflows/create-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+DATE=$(date +'%Y.%-m.%-d')
+SRC_TAR="toolchains_cc-$DATE.tar.gz"
+git archive --format=tar.gz --output=$SRC_TAR main
+gh release create "$DATE" \
+  $SRC_TAR \
+  --title "$DATE" \
+  --notes "### Installation
+\`\`\`
+bazel_dep(name = \"toolchains_cc\", version = \"$DATE\")
+register_toolchains(\"@toolchains_cc\")
+\`\`\`"
+
+# Output the tag for the workflow
+echo "release_tag=$DATE" >> $GITHUB_OUTPUT
+echo "release_source_tarball=$SRC_TAR" >> $GITHUB_ENV
